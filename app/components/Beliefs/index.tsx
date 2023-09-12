79e5816c3982b545dbe2595676ee7efdde3b25c1
Deniz Seyrekbasan <dseyrekbasan@papatia.co>
2023-09-12 12:13:26 +0300

    first-commit

---
+++ app/components/Beliefs/index.tsx
@@ -0,0 +1,45 @@
+const Beliefs = () => {
+  return (
+    <div className="mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8 rounded-3xl">
+      <div className="bg-build pt-12 px-10 sm:px-24 pb-52 md:pb-70 rounded-3xl">
+        {/* <h2 className="text-lg font-normal text-blue tracking-widest mb-5 text-center sm:text-start">
+          BUILD
+        </h2> */}
+        <h4 className="text-4xl sm:text-65xl font-bold text-black leading-snug mb-5 text-center sm:text-start">
+          "Online Beslenme Danışmanlığı:{" "}
+          <span className="text-blue">
+            Sağlıklı Yaşam Tarzını Destekleyen Dijital Rehberlik"
+          </span>
+        </h4>
+        <h4 className="text-2xl pt-2 mb-5 text-center sm:text-start">
+          Online beslenme danışmanlığı ile dilediğiniz zaman yanınızdayım !!!
+        </h4>
+        <h4 className="text-2xl pt-2 mb-5 text-center sm:text-start">
+          Benimle beraber sürece başlamaya karar verdiğiniz takdirde sizlere bir
+          anamnez formu iletiyorum. Bu form beslenme alışkanlıklarınızı ve
+          sizleri daha iyi tanımama olanak sağlıyor.
+        </h4>
+        <h4 className="text-2xl pt-2 mb-5 text-center sm:text-start">
+          Beraber belirlediğimiz gün ve saatte ister facetime, ister arama veya
+          whatsapp üzerinden size özel ilk listemizi oluşturuyoruz. Sonrasında
+          istek ve hedeflerimize göre listemizi güncelliyoruz.
+        </h4>
+        <h4 className="text-2xl pt-2 mb-5 text-center sm:text-start">
+          <span className="font-black">Yasaksız </span> ilerdiğimiz bu süreçte
+          tek yapmanız gereken benimle iletişime geçmek :)
+        </h4>
+        <h4 className="text-2xl pt-2 mb-5 text-center sm:text-start">
+          <span className="font-black">Peki Süreç Nasıl İlerliyor ?</span>
+        </h4>
+        <h4 className="text-2xl pt-2 mb-5 text-center sm:text-start"></h4>
+        {/* <div className="text-center sm:text-start">
+          <button className="text-xl py-5 px-14 mt-5 font-semibold text-white rounded-full bg-blue border border-blue hover:bg-hoblue">
+            Learn more
+          </button>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Beliefs;
